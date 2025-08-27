16dd41a789ad06a67243e2ac9e5370bb0db6a4c9
confidence <nsirimconfidence@yahoo.com>
2025-08-27 14:26:50 +0100

    This setting forces Next.js to use PostCSS instead of LightningCSS

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-    domains: ['res.cloudinary.com','example.com']
+  images: {
+    domains: ["res.cloudinary.com", "example.com"],
+  },
 
+  experimental: {
+    optimizeCss: false, // force Next.js + Tailwind to use PostCSS instead of LightningCSS
   },
 };
 
