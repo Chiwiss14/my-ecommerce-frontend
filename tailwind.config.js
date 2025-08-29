00454c85bb3e32ad99abaea56f308ffd01149c57
confidence <nsirimconfidence@yahoo.com>
2025-08-29 11:08:37 +0100

    used another tailwindCSS version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  future: {
-    cssEngine: "postcss", // 👈 this disables LightningCSS
-  },
-}
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+//   future: {
+//     cssEngine: "postcss", // 👈 this disables LightningCSS
+//   },
+// }
